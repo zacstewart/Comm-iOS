69ea0be365667cfd52e531bb7f6c1c99adbec430
Zac Stewart <zgstewart@gmail.com>
2016-07-24 18:29:23 -0400

    Initial Commit

---
+++ Comm/Comm.h
@@ -0,0 +1,19 @@
+//
+//  Comm.h
+//  Comm
+//
+//  Created by Zac Stewart on 7/24/16.
+//  Copyright © 2016 Zac Stewart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Comm.
+FOUNDATION_EXPORT double CommVersionNumber;
+
+//! Project version string for Comm.
+FOUNDATION_EXPORT const unsigned char CommVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Comm/PublicHeader.h>
+
+
